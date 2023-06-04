2648157486bfde592d4dcd8826f3f184ffb78479
mayucoisobe <121940353+mayucoisobe@users.noreply.github.com>
2023-06-04 16:15:55 +0900

    splashscreen 修正初回load時のみ

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,24 +1,21 @@
+'use client';
 import Image from 'next/image';
-import { useLayoutEffect } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import styles from '../styles/Splash.module.css';
-// 'use client';
 
 // GSAP のインポート
 // import gsap from 'gsap';
 import { gsap } from 'gsap';
 
-// Pluginのインポート
-// SSR doesn't work with ES Modules so we need to import the UMD files instead
-// import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+export default function SplashScreen({ finishLoading }) {
+  const [isMounted, setIsMounted] = useState(false);
 
-// Pluginの登録
-// gsap.registerPlugin(ScrollTrigger);
-// const tl = gsap.timeline();
+  const animation = () => {
+    const loader = gsap.timeline({
+      onComplete: () => finishLoading(),
+    });
 
-export default function Splash() {
-  useLayoutEffect(() => {
-    gsap
-      .timeline()
+    loader
       .to('.circle', { y: '-1rem', duration: 0.3, ease: 'power4.out' })
       .to('.circle', { y: '2.25rem', duration: 0.3, ease: 'power4.out' })
       .to('.circle', { y: '-1.05rem', duration: 0.3, ease: 'power4.out' })
@@ -91,10 +88,93 @@ export default function Splash() {
         },
         '>'
       );
-  }, []);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsMounted(true), 10);
+    animation();
+    return () => clearTimeout(timeout);
+  });
+
+  // useLayoutEffect(() => {
+  //   gsap
+  //     .timeline()
+  //     .to('.circle', { y: '-1rem', duration: 0.3, ease: 'power4.out' })
+  //     .to('.circle', { y: '2.25rem', duration: 0.3, ease: 'power4.out' })
+  //     .to('.circle', { y: '-1.05rem', duration: 0.3, ease: 'power4.out' })
+  //     .to('.triangle', {
+  //       y: '-6rem',
+  //       rotation: 30,
+  //       duration: 0.35,
+  //       ease: 'power4.out',
+  //     })
+  //     .to('.triangle', { x: '8rem', y: '13rem', rotation: 420, duration: 1.2, ease: 'bounce.out' })
+  //     .to(
+  //       '.triangle',
+  //       {
+  //         backgroundColor: '#fff',
+  //         border: '1px solid #3e87ff',
+  //         duration: 0.5,
+  //       },
+  //       '1.1'
+  //     )
+  //     .to(
+  //       '.ractan',
+  //       {
+  //         scaleY: '.6',
+  //         transformOrigin: '0% bottom',
+  //         duration: 0.3,
+  //       },
+  //       '0.37'
+  //     )
+  //     .to('.ractan', { scaleY: '1' }, '>')
+  //     .fromTo(
+  //       '.splashTtlInner',
+  //       {
+  //         background: 'radial-gradient(circle at center, #3e87ff 0%, transparent 0%)',
+  //       },
+  //       {
+  //         background: 'radial-gradient(circle at center, #3e87ff 85%, transparent 100%)',
+  //         fontSize: '3.7rem',
+  //         duration: 2,
+  //         ease: 'power4.out',
+  //       },
+  //       '1.1'
+  //     )
+  //     .to('.splashTtlInner', { background: '#3e87ff' }, '>')
+  //     .to(
+  //       '.circle',
+  //       {
+  //         scale: 3,
+  //         duration: 0.5,
+  //         ease: 'power4.out',
+  //       },
+  //       '1.5'
+  //     )
+  //     .to(
+  //       '.ractan',
+  //       {
+  //         y: '5.5rem',
+  //         border: '10px outset #ef476f',
+  //         ease: 'power4.out',
+  //       },
+  //       '1.6'
+  //     )
+  //     .to(
+  //       '.ractan',
+  //       {
+  //         x: '-15rem',
+  //         scale: 1.5,
+  //         transformOrigin: '50% 50%',
+  //         rotate: '-360deg',
+  //         duration: 1.3,
+  //       },
+  //       '>'
+  //     );
+  // }, []);
 
   return (
-    <div className={styles.splash}>
+    <div className={styles.splash} isMounted={isMounted}>
       <div>
         <div className={styles.splashTtl}>
           <h2 className={`splashTtlInner en ${styles.splashTtlInner}`}>
@@ -120,69 +200,3 @@ export default function Splash() {
     </div>
   );
 }
-
-// useLayoutEffect(() => {
-//   gsap
-//     .timeline()
-//     .to('.circle', { y: '-1rem', duration: 0.3, ease: 'power4.out' })
-//     .to('.circle', { y: '2.25rem', duration: 0.3, ease: 'power4.out' })
-//     .to('.circle', { y: '-1.05rem', duration: 0.3, ease: 'power4.out' })
-//     .to('.triangle', {
-//       y: '-6rem',
-//       rotation: 30,
-//       duration: 0.15,
-//       ease: 'power4.out',
-//     })
-//     .to('.triangle', { x: '8rem', y: '13rem', rotation: 420, duration: 1.2, ease: 'bounce.out' });
-
-//   gsap
-//     .timeline()
-//     .to('.ractan', {
-//       scaleY: '.6',
-//       transformOrigin: '0% bottom',
-//       duration: 0.3,
-//       delay: 0.37,
-//     })
-//     .to('.ractan', { scaleY: '1' });
-
-//   gsap
-//     .timeline()
-//     .fromTo(
-//       '.splashTtlInner',
-//       {
-//         background: 'radial-gradient(circle at center, #3e87ff 0%, transparent 0%)',
-//       },
-//       {
-//         background: 'radial-gradient(circle at center, #3e87ff 85%, transparent 100%)',
-//         fontSize: '3.7rem',
-//         duration: 2,
-//         ease: 'power4.out',
-//         delay: 1,
-//       }
-//     )
-//     .to('.splashTtlInner', { background: '#3e87ff' });
-
-//   gsap.to('.triangle', {
-//     backgroundColor: '#fff',
-//     border: '1px solid #3e87ff',
-//     delay: 1.3,
-//   });
-
-//   gsap.to('.circle', { scale: 3, duration: 0.5, ease: 'power4.out', delay: 1.5 });
-
-//   gsap
-//     .timeline()
-//     .to('.ractan', {
-//       y: '5.5rem',
-//       border: '10px outset #ef476f',
-//       ease: 'power4.out',
-//       delay: 1.6,
-//     })
-//     .to('.ractan', {
-//       x: '-15rem',
-//       scale: 1.5,
-//       transformOrigin: '50% 50%',
-//       rotate: '-360deg',
-//       duration: 1.3,
-//     });
-// }, []);
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,29 @@
+import { useEffect, useState } from 'react';
 import { Footer } from './Footer';
 import { Header } from './Header';
+import SplashScreen from './SplashScreen';
+import { usePathname } from 'next/navigation';
 
 export default function Layout({ children }) {
+  const pathname = usePathname();
+  const isHome = pathname === '/';
+  const [isLoading, setIsLoading] = useState(isHome);
+
+  useEffect(() => {
+    if (isLoading) return;
+  }, [isLoading]);
+
   return (
     <>
-      <Header />
-      <main className="sm:mt-auto">{children}</main>
-      <Footer />
+      {isLoading && isHome ? (
+        <SplashScreen finishLoading={() => setIsLoading(false)} />
+      ) : (
+        <>
+          <Header />
+          <main className="sm:mt-auto">{children}</main>
+          <Footer />
+        </>
+      )}
     </>
   );
 }
