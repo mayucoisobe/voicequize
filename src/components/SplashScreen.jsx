9c61b4aeb89a699c9cda67ad15f0165154da35df
mayucoisobe <121940353+mayucoisobe@users.noreply.github.com>
2023-06-05 01:24:35 +0900

    splash 少し修正

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -15,7 +15,7 @@ export default function SplashScreen({ finishLoading }) {
     loader
       .to('.circle', { y: '-1rem', duration: 0.3, ease: 'power4.out' })
       .to('.circle', { y: '2.25rem', duration: 0.3, ease: 'power4.out' })
-      .to('.circle', { y: '-1.05rem', duration: 0.3, ease: 'power4.out' })
+      .to('.circle', { y: '-1.3rem', duration: 0.3, ease: 'power4.out' })
       .to('.triangle', { y: '-6.5rem', rotation: 30, duration: 0.35, ease: 'power4.out' })
       .to('.triangle', { x: '8rem', y: '13rem', rotation: 420, duration: 1.2, ease: 'bounce.out' })
       .to('.triangle', { backgroundColor: '#fff', border: '1px solid #3e87ff', duration: 0.5 }, '1.1')
@@ -25,7 +25,7 @@ export default function SplashScreen({ finishLoading }) {
         '.splashTtlInner',
         { background: 'radial-gradient(circle at center, #3e87ff 0%, transparent 0%)' },
         {
-          background: 'radial-gradient(circle at center, #3e87ff 85%, transparent 100%)',
+          background: 'radial-gradient(circle at center, #3e87ff 95%, transparent 100%)',
           fontSize: '3.7rem',
           duration: 2,
           ease: 'power4.out',
@@ -35,7 +35,8 @@ export default function SplashScreen({ finishLoading }) {
       .to('.splashTtlInner', { background: '#3e87ff' }, '>')
       .to('.circle', { scale: 3, duration: 0.5, ease: 'power4.out' }, '1.5')
       .to('.ractan', { y: '5.5rem', border: '10px outset #ef476f', ease: 'power4.out' }, '1.6')
-      .to('.ractan', { x: '-15rem', scale: 1.5, transformOrigin: '50% 50%', rotate: '-360deg', duration: 1.3 }, '>');
+      .to('.ractan', { x: '-15rem', scale: 2, transformOrigin: '50% 50%', rotate: '-360deg', duration: 1.3 }, '-=2')
+      .to('.circle', { transformOrigin: '50% 50%', rotate: '15', duration: 0.2, ease: 'power4.out' }, '-=1');
   };
 
   useEffect(() => {
