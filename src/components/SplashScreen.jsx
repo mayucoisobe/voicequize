f395bca11820906e5e87b2a579e626c566369658
mayucoisobe <121940353+mayucoisobe@users.noreply.github.com>
2023-06-04 17:05:30 +0900

    見切れる不具合　高さ修正etc

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -20,7 +20,7 @@ export default function SplashScreen({ finishLoading }) {
       .to('.circle', { y: '2.25rem', duration: 0.3, ease: 'power4.out' })
       .to('.circle', { y: '-1.05rem', duration: 0.3, ease: 'power4.out' })
       .to('.triangle', {
-        y: '-6rem',
+        y: '-6.5rem',
         rotation: 30,
         duration: 0.35,
         ease: 'power4.out',
@@ -95,83 +95,6 @@ export default function SplashScreen({ finishLoading }) {
     animation();
     return () => clearTimeout(timeout);
   });
-
-  // useLayoutEffect(() => {
-  //   gsap
-  //     .timeline()
-  //     .to('.circle', { y: '-1rem', duration: 0.3, ease: 'power4.out' })
-  //     .to('.circle', { y: '2.25rem', duration: 0.3, ease: 'power4.out' })
-  //     .to('.circle', { y: '-1.05rem', duration: 0.3, ease: 'power4.out' })
-  //     .to('.triangle', {
-  //       y: '-6rem',
-  //       rotation: 30,
-  //       duration: 0.35,
-  //       ease: 'power4.out',
-  //     })
-  //     .to('.triangle', { x: '8rem', y: '13rem', rotation: 420, duration: 1.2, ease: 'bounce.out' })
-  //     .to(
-  //       '.triangle',
-  //       {
-  //         backgroundColor: '#fff',
-  //         border: '1px solid #3e87ff',
-  //         duration: 0.5,
-  //       },
-  //       '1.1'
-  //     )
-  //     .to(
-  //       '.ractan',
-  //       {
-  //         scaleY: '.6',
-  //         transformOrigin: '0% bottom',
-  //         duration: 0.3,
-  //       },
-  //       '0.37'
-  //     )
-  //     .to('.ractan', { scaleY: '1' }, '>')
-  //     .fromTo(
-  //       '.splashTtlInner',
-  //       {
-  //         background: 'radial-gradient(circle at center, #3e87ff 0%, transparent 0%)',
-  //       },
-  //       {
-  //         background: 'radial-gradient(circle at center, #3e87ff 85%, transparent 100%)',
-  //         fontSize: '3.7rem',
-  //         duration: 2,
-  //         ease: 'power4.out',
-  //       },
-  //       '1.1'
-  //     )
-  //     .to('.splashTtlInner', { background: '#3e87ff' }, '>')
-  //     .to(
-  //       '.circle',
-  //       {
-  //         scale: 3,
-  //         duration: 0.5,
-  //         ease: 'power4.out',
-  //       },
-  //       '1.5'
-  //     )
-  //     .to(
-  //       '.ractan',
-  //       {
-  //         y: '5.5rem',
-  //         border: '10px outset #ef476f',
-  //         ease: 'power4.out',
-  //       },
-  //       '1.6'
-  //     )
-  //     .to(
-  //       '.ractan',
-  //       {
-  //         x: '-15rem',
-  //         scale: 1.5,
-  //         transformOrigin: '50% 50%',
-  //         rotate: '-360deg',
-  //         duration: 1.3,
-  //       },
-  //       '>'
-  //     );
-  // }, []);
 
   return (
     <div className={styles.splash} isMounted={isMounted}>
