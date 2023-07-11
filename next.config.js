93dfbb64e48086030fbc5ffe1f07cf5f09b1f85a
mayucoisobe <121940353+mayucoisobe@users.noreply.github.com>
2023-07-11 14:17:20 +0900

    next config 再修正

--- next.config.js
+++ next.config.js
@@ -1,17 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});
+
 // const nextConfig = {
 //   reactStrictMode: true,
 // };
 
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  },
-  reactStrictMode: true,
-});
+// const withPWA = require('next-pwa');
+// module.exports = withPWA({
+//   pwa: {
+//     dest: 'public',
+//     register: true,
+//     skipWaiting: true,
+//   },
+//   reactStrictMode: true,
+// });
 
 // module.exports = nextConfig;
