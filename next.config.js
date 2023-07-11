ee93e9f0e6a2a0ea4886d7e8120171f06dd3a120
mayucoisobe <121940353+mayucoisobe@users.noreply.github.com>
2023-07-11 14:06:51 +0900

    next config 再度修正試みる

--- next.config.js
+++ next.config.js
@@ -1,30 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
 
 const withPWA = require('next-pwa');
+
 module.exports = withPWA({
   pwa: {
     dest: 'public',
     register: true,
     skipWaiting: true,
-    // runtimeCaching: [
-    //   {
-    //     urlPattern: /^https:\/\/voicequize.vercel.app\/.*/,
-    //     handler: 'NetworkFirst',
-    //     options: {
-    //       cacheName: 'static-cache',
-    //       expiration: {
-    //         maxEntries: 10,
-    //         maxAgeSeconds: 24 * 60 * 60, // 24 hours
-    //       },
-    //       networkTimeoutSeconds: 10,
-    //     },
-    //   },
-    // ],
   },
   reactStrictMode: true,
 });
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
