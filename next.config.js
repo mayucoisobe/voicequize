ecf14b994f3c4b376e2efd1a94f149a318aa20c4
mayucoisobe <121940353+mayucoisobe@users.noreply.github.com>
2023-07-11 11:34:40 +0900

    next config cache 追加

--- next.config.js
+++ next.config.js
@@ -9,6 +9,20 @@ module.exports = withPWA({
     dest: 'public',
     register: true,
     skipWaiting: true,
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/voicequize.vercel.app\/.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'static-cache',
+          expiration: {
+            maxEntries: 10,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+          networkTimeoutSeconds: 10,
+        },
+      },
+    ],
   },
   reactStrinctMode: true,
 });
